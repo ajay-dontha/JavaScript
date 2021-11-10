048ca7ee1fbd3a4ce18f17357780e1aa434b2475
ajay-developer <84758365+ajay-dontha@users.noreply.github.com>
2021-11-10 22:51:02 +0530

    Add files via upload

---
+++ JS_Tasks/10_Task_forloop/js/script.js
@@ -0,0 +1,24 @@
+// print num from 45-100
+console.log("print num from: 5-10")
+
+for(var i=5;i<=10;i++)
+{
+    console.log("num"+i);
+}
+
+// print num from 45-100
+console.log("print num from: 45-100")
+
+for(var i=45;i<=100;i++)
+{
+    console.log("num"+i)
+}
+
+// print num from 10-0
+console.log("print number from: 10-0")
+
+for(var i=10;i>=0;i--)
+{
+    console.log("num"+i);
+}
+
