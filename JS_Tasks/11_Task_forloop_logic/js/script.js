22258f25f71eee537d4dfe02f8dcb60fc7d05b07
ajay-developer <84758365+ajay-dontha@users.noreply.github.com>
2021-11-15 09:52:28 +0530

    Add files via upload

---
+++ JS_Tasks/11_Task_forloop_logic/js/script.js
@@ -0,0 +1,19 @@
+console.log("For loop:fruits, fruit salad, fruit salad recipe");
+
+for(let i=1;i<=5;i++)
+{
+        if (i%2==0) 
+         {
+              console.log("Fruits:"+i);
+         }
+
+        if (i%3==0)       
+         {
+              console.log("Fruit Salad:"+i);   
+         }
+         
+         if (i%5==0)
+         {
+             console.log("Fruit Salad Recipe:"+i);
+         }
+}
