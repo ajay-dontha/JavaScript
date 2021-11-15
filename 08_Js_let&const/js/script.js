93b132e983deb9e17d6c0f10a4ba24b8a151943c
ajay-developer <84758365+ajay-dontha@users.noreply.github.com>
2021-11-15 09:53:19 +0530

    Add files via upload

---
+++ 08_Js_let&const/js/script.js
@@ -0,0 +1,13 @@
+console.log("Let and Const keywords:")
+
+let a=10;
+console.log("The value a:"+a);
+
+a=20;
+console.log("The value a:"+a);
+
+const i=10;
+console.log("The value i:"+i);
+
+i=20;
+console.log("The value i:"+i);
---
+++ 09_Js_alert&Promt/js/script.js
@@ -0,0 +1,7 @@
+console.log("Alert & Prompt:")
+//alert box
+alert("alert box");
+
+//prompt
+var value=prompt("enter any value");
+console.log("value:"+value);
